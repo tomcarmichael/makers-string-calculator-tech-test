c9a8b7e7e5b558318caa0a811bd02043df119bbe
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-07-27 16:31:33 +0100

    Refactored function to dynamically return based on input arg gien just one Number as input

--- string_calculator.py
+++ string_calculator.py
@@ -1,7 +1,2 @@
 def string_calculator(input_string):
-    if input_string == '1':
-        return ["1", 1.0]
-    elif input_string == '2':
-        return ["2", 2.0]
-    else:
-        return ["10", 10.0]
+    return [input_string, float(input_string)]
