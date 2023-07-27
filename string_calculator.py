5713aed795418c761b0131b2310191e50074dbcd
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-07-27 16:27:22 +0100

    Created 2nd passing test for single number input using conditional explicit return

--- string_calculator.py
+++ string_calculator.py
@@ -1,2 +1,5 @@
 def string_calculator(input_string):
-    return ["1", 1.0]
+    if input_string == '1':
+        return ["1", 1.0]
+    else:
+        return ["2", 2.0]
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -4,3 +4,7 @@ import pytest
 def test_string_calculator_1():
     input = '1'
     assert(string_calculator(input)) == ["1", 1.0]
+
+def test_string_calculator_2():
+    input = '2'
+    assert(string_calculator(input)) == ["2", 2.0]
