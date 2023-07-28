abd209ec21478dccbbb7fe728c4d8cdf8672e81a
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-07-28 12:58:13 +0100

    Refactored to reduce calls to float()

--- string_calculator.py
+++ string_calculator.py
@@ -3,15 +3,17 @@ def string_calculator(input_string):
 
     if len(input_array) == 1:
         return [input_string, float(input_string)]
+    
+    first_term = float(input_array[0])
+    second_term = float(input_array[2])
+
+    if input_array[1] == '+':
+        result = first_term + second_term
+    elif input_array[1] == '-':
+        result = first_term - second_term
+    elif input_array[1] == '/':
+        result = first_term / second_term
     else:
-        if input_array[1] == '+':
-            result = float(input_array[0]) + float(input_array[2])
-        elif input_array[1] == '-':
-            result = float(input_array[0]) - float(input_array[2])
-        elif input_array[1] == '/':
-            result = float(input_array[0]) / float(input_array[2])
-        else:
-            result = float(input_array[0]) * float(input_array[2])
+        result = first_term * second_term
 
-
-        return [input_string, result]
+    return [input_string, result]
